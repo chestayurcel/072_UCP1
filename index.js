ed50f74ef2829f4c3433321360681989def630cc
Yurcell <chestayurcel@gmail.com>
2025-11-03 08:47:06 +0700

    membuat method DELETE untuk menghapus data pada endpoint /buku/:id

--- index.js
+++ index.js
@@ -54,3 +54,18 @@ app.put('/buku/:id', async (req, res) => {
         res.status(500).send(err);
     }
 });
+
+app.delete('/buku/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const buku = await db.Buku.findByPk(id);
+        if (!buku) {
+            return res.status(404).send({ message: "Buku tidak ditemukan!" });
+        }
+
+        await buku.destroy();
+        res.send({ message: "Buku berhasil dihapus!" });
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
